6c30ce95ec397eca005c1ef6787ce8cb62cc2df5
Bhui <Bhui@KIRBYZ240W10-2.ad.nchecr.unsw.edu.au>
2018-06-12 15:07:36 +1000

    Added code to calculate incidences

--- test/test/Test_Population_Remote_MetaPopulation_Pop_IntroSyphilis.java
+++ test/test/Test_Population_Remote_MetaPopulation_Pop_IntroSyphilis.java
@@ -1,35 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
 import java.io.File;
 import java.io.IOException;
-import run.Run_Population_Remote_MetaPopulation_Pop_IntroSyphilis;
+import run.Run_Population_Remote_MetaPopulation_Pop_Intro_Syphilis;
 
 public class Test_Population_Remote_MetaPopulation_Pop_IntroSyphilis {
 
     public static void main(String[] arg) {
 
-        String[] rArg = new String[]{
-            // Base Dir            
-            "C:\\Users\\Bhui\\OneDrive - UNSW\\RMP\\Gen_Results_500_Syphilis_DoubleDuration",
-            // Import Dir
-            "C:\\Users\\Bhui\\OneDrive - UNSW\\RMP\\ImportDir",
-            // Num threads
-            "8",
-            // Num sim
-            "500",};
+        String[] baseDirStrCollection = new String[]{
+            "C:\\Users\\Bhui\\OneDrive - UNSW\\RMP\\Gen_Results_1000_Syphilis_Var_Trans_CSV",
+            "C:\\Users\\Bhui\\OneDrive - UNSW\\RMP\\Gen_Results_1000_Syphilis_CSV",
+            "C:\\Users\\Bhui\\OneDrive - UNSW\\RMP\\Gen_Results_1000_Syphilis_Half_Trans_CSV",   
+            //"C:\\Users\\Bhui\\Desktop\\FTP\\RMP\\Syphilis\\Gen_Results_1000_Syphilis",
+        };
 
-        Run_Population_Remote_MetaPopulation_Pop_IntroSyphilis run = new Run_Population_Remote_MetaPopulation_Pop_IntroSyphilis(rArg);
-        run.runSimulation();
+        for (String baseDirStr : baseDirStrCollection) {
 
-        try {
-            Run_Population_Remote_MetaPopulation_Pop_IntroSyphilis.decodeCollectionFile(new File(rArg[0]));
-        } catch (IOException | ClassNotFoundException ex) {
-            ex.printStackTrace(System.err);
+            String[] rArg = new String[]{
+                // Base Dir            
+                baseDirStr,
+                // Import Dir
+                "C:\\Users\\Bhui\\OneDrive - UNSW\\RMP\\ImportDir",
+                // Num threads
+                "8",
+                // Num sim
+                "1000",
+                // Num Step
+                "18000",
+                // Sample Freq
+                "",
+                // Store infection history
+                "false"
+            };
+
+            File baseDir = new File(baseDirStr);
+            if (baseDir.exists() && baseDir.listFiles().length > 1) {
+                System.out.println(baseDir.getAbsolutePath() + " has more than 1 file inside. Simulation NOT Run");
+            } else {
+                Run_Population_Remote_MetaPopulation_Pop_Intro_Syphilis run = new Run_Population_Remote_MetaPopulation_Pop_Intro_Syphilis(rArg);
+                run.runSimulation();
+            }
+
+            try {
+                Run_Population_Remote_MetaPopulation_Pop_Intro_Syphilis.decodeCollectionFile(baseDir);
+            } catch (IOException | ClassNotFoundException ex) {
+                ex.printStackTrace(System.err);
+            }
         }
 
     }
--- test/test/Test_Population_Remote_MetaPopulation_Pop_Optimisation.java
+++ test/test/Test_Population_Remote_MetaPopulation_Pop_Optimisation.java
@@ -9,13 +9,13 @@ import opt.OptRun_Population_Remote_MetaPopulation_Infection_Intro_GA;
 public class Test_Population_Remote_MetaPopulation_Pop_Optimisation {
      public static void main(String[] arg) throws IOException, ClassNotFoundException, InterruptedException {
          
-         boolean useGA = true;
+         boolean useGA = !true;
          
          
          String[] rArg = new String[]{
              // Base Dir             
              //"C:\\Users\\Bhui\\Desktop\\FTP\\RMP\\OptResults_GA", 
-             "C:\\Users\\Bhui\\OneDrive - UNSW\\RMP\\OptResults_GA",
+             "C:\\Users\\Bhui\\Desktop\\FTP\\RMP\\Opt_NG_CT",
              // Import Dir
              "C:\\Users\\Bhui\\OneDrive - UNSW\\RMP\\ImportDir", 
              // Num threads
@@ -31,7 +31,7 @@ public class Test_Population_Remote_MetaPopulation_Pop_Optimisation {
          };
          
          if(!useGA){         
-             rArg[0] = "C:\\Users\\Bhui\\Desktop\\FTP\\RMP\\OptResults";
+             //rArg[0] = "C:\\Users\\Bhui\\Desktop\\FTP\\RMP\\OptResults";
              OptRun_Population_Remote_MetaPopulation_Infection_Intro.main(rArg);
          }else{
              OptRun_Population_Remote_MetaPopulation_Infection_Intro_GA.main(rArg);
