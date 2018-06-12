ed8326da996e3e9fb0b5fa7e8b02d5ea6b9df3dc
Bhui <Bhui@KIRBYZ240W10-2.ad.nchecr.unsw.edu.au>
2018-06-12 16:01:34 +1000

    Added code to calculate incidences

--- test/test/Test_Population_Remote_MetaPopulation_Sim.java
+++ test/test/Test_Population_Remote_MetaPopulation_Sim.java
@@ -8,7 +8,7 @@ public class Test_Population_Remote_MetaPopulation_Sim {
     public static void main(String[] arg) throws IOException, InterruptedException, ClassNotFoundException {
         String[] baseDir = new String[]{
             "C:\\Users\\Bhui\\OneDrive - UNSW\\RMP\\NG_CT", 
-            "Srn_Baseline", //"Srn_POC"
+            "Srn_Baseline", "Srn_POC"
         };        
         
         Simulation_Remote_MetaPopulation.main(baseDir);
