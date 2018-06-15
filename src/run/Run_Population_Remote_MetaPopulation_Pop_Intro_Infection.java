5609db04f57dc0a30b8d3a6f23783d41a5176198
bbcbh <39325597+bbcbh@users.noreply.github.com>
2018-06-15 23:45:31 +1000

    Combine simulation and optimisaiton as one run file

--- src/run/Run_Population_Remote_MetaPopulation_Pop_Intro_Infection.java
+++ src/run/Run_Population_Remote_MetaPopulation_Pop_Intro_Infection.java
@@ -1,11 +1,24 @@
 package run;
 
+import java.io.IOException;
 import java.util.Arrays;
+import opt.OptRun_Population_Remote_MetaPopulation_Infection_Intro;
+import opt.OptRun_Population_Remote_MetaPopulation_Infection_Intro_GA;
+import sim.Simulation_Remote_MetaPopulation;
 
 /**
  * A common run file for all infection
  *
  * @author Ben Hui
+ * @version 20180615
+ * 
+ * <pre>
+ * History:
+ * 
+ * 20180615 
+ *  - Combine simulation and optimisaiton as one run file
+ * </pre>
+ * 
  */
 public class Run_Population_Remote_MetaPopulation_Pop_Intro_Infection {
 
@@ -26,10 +39,30 @@ public class Run_Population_Remote_MetaPopulation_Pop_Intro_Infection {
             case 1:
                 Run_Population_Remote_MetaPopulation_Pop_Intro_Syphilis runSyp = new Run_Population_Remote_MetaPopulation_Pop_Intro_Syphilis(rArg);
                 runSyp.runSimulation();
-
+                break;
+            case 2:
+                try {
+                    Simulation_Remote_MetaPopulation.main(rArg);
+                } catch (IOException | InterruptedException | ClassNotFoundException ex) {
+                    ex.printStackTrace(System.err);
+                }
+                break;
+            case 3:
+                try {
+                    OptRun_Population_Remote_MetaPopulation_Infection_Intro.main(rArg);
+                } catch (IOException | ClassNotFoundException ex) {
+                    ex.printStackTrace(System.err);
+                }
+                break;
+            case 4:
+                try {
+                    OptRun_Population_Remote_MetaPopulation_Infection_Intro_GA.main(rArg);
+                } catch (IOException | ClassNotFoundException ex) {
+                    ex.printStackTrace(System.err);
+                }
                 break;
             default:
-                System.err.println("Error: Illegal arg[0]. Set 0 for NG/CT and 1 for Syphilis");
+                System.err.println("Error: Illegal arg[0]. Set 0 for NG/CT Run, 1 for Syphilis, 2 for Simulation, 3 for Optimisation, 4 for Optimisaiton GA");
         }
 
     }
